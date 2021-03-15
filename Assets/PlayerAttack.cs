f9266588deba7777c219f5415b73aabb21bfbe4c
F. Kuhlmann <36305934+Thiiamas@users.noreply.github.com>
2021-03-15 13:57:49 +0100

    Initial commi
    Mostly empty project, base for testing

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack: MonoBehaviour
+{
+    public Animator animator;
+
+    float iAttackSpeed = 2.0f;
+    float attackSpeed;
+    float lastAttack = 0.0f;
+    private void Start()
+    {
+        attackSpeed = iAttackSpeed;
+        animator.SetFloat("attackSpeedMulti", attackSpeed/2.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && (Time.time >= lastAttack + (1/attackSpeed)))
+        {
+            basicAttack();
+            lastAttack = Time.time;
+        }
+
+        //Force the animation to transition after 1 loop for attack air
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("AttackAir"))
+        {
+            animator.SetBool("isAttacking", true);
+            if (this.animator.GetCurrentAnimatorStateInfo(0).normalizedTime > this.animator.GetCurrentAnimatorStateInfo(0).length)
+            {
+                animator.SetBool("isAttacking", false);
+            }
+        }
+
+    }
+
+    void basicAttack()
+    {
+        animator.SetTrigger("Attack");
+        attackSpeed = attackSpeed +0.5f;
+      /*  if (attackSpeed < 15f)
+        {
+            increaseAttackSpeed(0.5f);
+        }*/
+    }
+
+    void increaseAttackSpeed(float value)
+    {
+        attackSpeed = attackSpeed + 0.5f;
+        animator.SetFloat("attackSpeedMulti", attackSpeed / iAttackSpeed);
+    }
+}
---
+++ Assets/Player_Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float maxHealth = 100;
+    float health;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        Debug.Log("Player is dead");
+    }
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
