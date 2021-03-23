ccedc7eb93bee58c924d6a8c59e8d951ce07a38d
F. Kuhlmann <36305934+Thiiamas@users.noreply.github.com>
2021-03-23 15:19:01 +0100

    Update PlayerAttack.cs

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -46,8 +46,8 @@ public class PlayerAttack: MonoBehaviour
 
             foreach(Collider2D enemy in hitEnemies)
             {
-/*                enemy.GetComponentInParent<Boss>().TakeDamage(basicAttackDamage);
-*/            }
+                enemy.GetComponentInParent<Boss>().TakeDamage(basicAttackDamage);
+            }
 
             nextAttackTime = Time.time + 1/attackSpeed;
         }
